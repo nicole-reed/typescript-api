e1d23c68fdfde0cb2894c8537f2c391f50346774
Nicole Reed <nickreed033@gmail.com>
2022-07-29 14:55:24 +0600

    created handler wrapper

--- src/app.ts
+++ src/app.ts
@@ -1,34 +1,23 @@
-import express, { NextFunction, Request, Response } from "express";
+import express from "express";
 import { userController } from "./controllers/user.controller";
+import { httpHandler } from "./wrappers/handler.wrapper";
 
 const app = express();
 const port = 3000;
 
-app.get("/", async (req: Request, res: Response, next: NextFunction) => {
-    try {
-        res.send("1 Rep Max Tracker Homepage");
-    } catch (error) {
-        next(error);
-    }
-});
+app.get("/", httpHandler(async (req, res) => {
+    res.send("homepage");
+}));
 
-app.get("/users", async (req: Request, res: Response, next: NextFunction) => {
-    try {
-        const { body, status } = await userController.getUsers();
-        res.status(status).send(body);
-    } catch (error) {
-        next(error);
-    }
-});
+app.get("/users", httpHandler(async (req, res) => {
+    const { body, status } = await userController.getUsers();
+    res.status(status).send(body);
+}));
 
-app.get("/users/:id", async (req: Request, res: Response, next: NextFunction) => {
-    try {
-        const { body, status } = await userController.getUser(req);
-        res.status(status).send(body);
-    } catch (error) {
-        next(error);
-    }
-});
+app.get("/users/:id", httpHandler(async (req, res) => {
+    const { body, status } = await userController.getUser(req);
+    res.status(status).send(body);
+}));
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`);
