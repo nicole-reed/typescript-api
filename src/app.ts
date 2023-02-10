b8aa7f0acadae98264c791d7e8eb1ebb5f758b39
Nicole Reed <nickreed033@gmail.com>
2023-02-10 16:33:49 +0400

    added yaml file to deploy to app engine

--- src/app.ts
+++ src/app.ts
@@ -12,8 +12,10 @@ import { loginUser } from "./handlers/loginUser";
 import { authenticateUser } from "./middleware/authMiddleware";
 import "dotenv/config";
 
+const port = Number(process.env.PORT) || 8080;
+
 const app = express();
-const port = 3000;
+app.enable("trust proxy");
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
